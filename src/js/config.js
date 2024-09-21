cbfea19bfe27ad558aed9305301f64f1fefc2a8a
Gautam48 <gmocial48@gmail.com>
2024-09-21 16:15:00 +0530

    forkify-newKey

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "027a3aa6-92a6-42b9-b496-49e08b6d59a8";
+export const KEY = "702e93c0-374a-4aeb-bcdd-ceeed32907ad"; 
